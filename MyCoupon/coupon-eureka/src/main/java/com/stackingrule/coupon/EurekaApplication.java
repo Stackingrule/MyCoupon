6e1dd64bd20be7e9a9ab37c33a92522d70dfb65e
Stackingrule <messageswitching@gmail.com>
2020-02-21 18:25:01 +0800

    init coupon-eureka module

---
+++ MyCoupon/coupon-eureka/src/main/java/com/stackingrule/coupon/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.stackingrule.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * <h1>Eureka启动入口</h1>
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
