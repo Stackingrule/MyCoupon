38294aa3354c8a6f5c61dfb5ec82c26d6c99ba67
Stackingrule <messageswitching@gmail.com>
2020-03-05 22:26:13 +0800

    Init coupon-distribution Project

---
+++ MyCoupon/coupon-service/coupon-distribution/src/main/java/com/stackingrule/coupon/DistributionApplication.java
@@ -0,0 +1,33 @@
+package com.stackingrule.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * <h1>分发系统的启动入口</h1>
+ */
+@EnableJpaAuditing
+@EnableFeignClients
+@EnableCircuitBreaker
+@EnableEurekaClient
+@SpringBootApplication
+public class DistributionApplication {
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributionApplication.class, args);
+    }
+
+}
