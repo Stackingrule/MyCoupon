8a7b8ce90e9c5882f41e3a5988209e0f5b4187cd
Stackingrule <messageswitching@gmail.com>
2020-03-06 23:11:51 +0800

    根据用户 id 查找当前可以领取的优惠券模板

--- MyCoupon/coupon-service/coupon-distribution/src/main/java/com/stackingrule/coupon/service/impl/UserServiceImpl.java
+++ MyCoupon/coupon-service/coupon-distribution/src/main/java/com/stackingrule/coupon/service/impl/UserServiceImpl.java
@@ -13,12 +13,25 @@ import com.stackingrule.coupon.service.IUserService;
 import com.stackingrule.coupon.vo.*;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+
+import static javafx.util.Pair.*;
 
 /**
  * <h1>用户服务接口实现</h1>
@@ -145,18 +158,84 @@ public class UserServiceImpl implements IUserService {
         return preTarget;
     }
 
+    /**
+     * <h2>根据用户 id 查找当前可以领取的优惠券模板</h2>
+     * @param userId 用户 id
+     * @return
+     * @throws CouponException
+     */
     @Override
-    public List<CouponTemplateSDK> findAvailableTemplate(Long userId) throws CouponException {
+    public List<CouponTemplateSDK> findAvailableTemplate(Long userId)
+            throws CouponException {
+
+        long curTime = new Date().getTime();
+
+        List<CouponTemplateSDK> templateSDKS = templateClient.findAllUsableTemplate()
+                .getData();
+
+        log.debug("Find All Template(From TemplateClient) Count: {}",
+                templateSDKS.size());
+
+        // 过滤过期的优惠券模板
+        templateSDKS.stream().filter(
+                t -> t.getRule().getExpiration()
+                        .getDeadLine() > curTime)
+                .collect(Collectors.toList()
+                );
+
+        log.info("Find Usable Template Count: {}", templateSDKS.size());
+
+        // key 是 TemplateId
+        // value 中的 left 是 Template limitation, right 是优惠券模板
+        Map<Integer, Pair<Integer, CouponTemplateSDK>> limit2Template =
+                new HashMap<>(templateSDKS.size());
+
+        templateSDKS.forEach(
+                t -> limit2Template.put(
+                        t.getId(),
+                        org.apache.commons.lang3.tuple.Pair.of(t.getRule().getLimitation(), t)
+                )
+        );
+
+        List<CouponTemplateSDK> result = new ArrayList<>(limit2Template.size());
+
+        List<Coupon> userUsableCoupons = findCouponByStatus(userId,
+                CouponStatus.USABLE.getCode());
+
+        log.debug("Current User Has Usable Coupons: {}, {}", userId,
+                userUsableCoupons.size());
+
+        // key 是 TemplateId
+        Map<Integer, List<Coupon>> templateId2Coupons = userUsableCoupons
+                .stream()
+                .collect(Collectors.groupingBy(Coupon::getTemplateId));
+
+        // 根据 Template 的 Rule 判断是否可以领取优惠券模板
+        limit2Template.forEach((k, v) -> {
+
+            int limitation = v.getLeft();
+            CouponTemplateSDK templateSDK = v.getRight();
+
+            if (templateId2Coupons.containsKey(k)
+                    && templateId2Coupons.get(k).size() >= limitation) {
+                return;
+            }
+
+            result.add(templateSDK);
+        });
+
+        return result;
+    }
+
+    @Override
+    public Coupon acquireTemplate(AcquireTemplateRequest request)
+            throws CouponException {
         return null;
     }
 
     @Override
-    public Coupon acquireTemplate(AcquireTemplateRequest request) throws CouponException {
-        return null;
-    }
-
-    @Override
-    public SettlementInfo settlement(SettlementInfo info) throws CouponException {
+    public SettlementInfo settlement(SettlementInfo info)
+            throws CouponException {
         return null;
     }
 }
